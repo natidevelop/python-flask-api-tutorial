10fa999c72619411138d1114f5c56db134c5e016
natidevelop <163793240+natidevelop@users.noreply.github.com>
2024-11-11 22:05:59 +0000

    ejercicio finalizado

--- src/app.py
+++ src/app.py
@@ -12,11 +12,19 @@ todos = [
 def hello_world():
     json_text = jsonify(todos)
     return json_text
+
 @app.route('/todos', methods=['POST'])
 def add_new_todo():
     request_body = request.json
-    print("Incoming request with the following body", request_body)
-    return 'Response for the POST todo'
+    todos.append(request_body)
+    
+    return jsonify(todos),200
+
+@app.route('/todos/<int:position>', methods=['DELETE'])
+def delete_todo(position):
+    todos.pop(position-1)
+    return jsonify(todos),200
+
 
  
 if __name__ == '__main__':
